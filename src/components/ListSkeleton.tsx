192f0cd0e67a1462915705d5e681407b941a671c
Viktor Skarlatov <viktor.skarlatov@themakergroup.com>
2026-06-10 13:17:56 +0300

    rename variable

--- src/components/ListSkeleton.tsx
+++ src/components/ListSkeleton.tsx
@@ -1,11 +1,11 @@
 import { List, ListItem, Skeleton } from "@mui/material";
 
-const SKELETON_COUNT = 8
+const ITEM_COUNT = 8
 
 export function ListSkeleton() {
   return (
     <List>
-      {Array.from({ length: SKELETON_COUNT }).map((_, index) => (
+      {Array.from({ length: ITEM_COUNT }).map((_, index) => (
         <ListItem key={index} divider>
           <Skeleton variant="text" width="60%" height={32} />
         </ListItem>
